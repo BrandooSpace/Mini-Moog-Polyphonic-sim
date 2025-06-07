b9ef621fd12770d5c36bb795f93848b9c96ff191
BrandooSpace <bfinch003@gmail.com>
2025-06-06 22:58:04 -0400

    config again

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, process.cwd(), '');
+
     return {
+      // The base path for the deployed site
+      base: '/Mini-Moog-Polyphonic-sim/',
+
+      // The list of plugins
+      plugins: [react()],
+
+      // Global constant replacements
       define: {
-        base: '/Mini-Moog-Polyphonic-sim/',
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+
+      // Path resolver aliases
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
